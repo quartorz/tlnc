b261067a5d4e47c55c70eafbabf178d503815446
n1211223 <n1211223@edu.cc.uec.ac.jp>
2015-07-23 14:51:03 +0900

    add comment

--- include/tlnc/expressions/constant.hpp
+++ include/tlnc/expressions/constant.hpp
@@ -35,12 +35,14 @@ namespace tlnc{
 }
 
 // make double constant
+// TLNC_C(x)
 #define TLNC_C(x) ::tlnc::expressions::constant<BCL_DOUBLE_T(x)>{}
 
 #define TLNC_I_I_1(x) ::tlnc::expressions::constant<::cti::interval<BCL_DOUBLE_T(x), BCL_DOUBLE_T(x)>>{}
 #define TLNC_I_I_2(x, y) ::tlnc::expressions::constant<::cti::interval<BCL_DOUBLE_T(x), BCL_DOUBLE_T(y)>>{}
 
 // make interval constant
+// TLNC_I(x) or TLNC_I(inf, sup)
 #if defined(_MSC_VER)
 # define TLNC_I(...) BOOST_PP_CAT(BOOST_PP_OVERLOAD(TLNC_I_I_,__VA_ARGS__)(__VA_ARGS__),BOOST_PP_EMPTY())
 #else
@@ -48,11 +50,13 @@ namespace tlnc{
 #endif
 
 // alias to TLNC_C
+// t@github.com:quartorz/bcl.gitC_
 #if !defined(C_)
 # define C_(x) TLNC_C(x)
 #endif
 
 // alias to TLNC_I
+// I_(x) or I_(inf, sup)
 #if !defined(I_) && !defined(I_I_1) && !defined(I_I_2)
 # define I_I_1(x) ::tlnc::expressions::constant<::cti::interval<BCL_DOUBLE_T(x), BCL_DOUBLE_T(x)>>{}
 # define I_I_2(x, y) ::tlnc::expressions::constant<::cti::interval<BCL_DOUBLE_T(x), BCL_DOUBLE_T(y)>>{}
