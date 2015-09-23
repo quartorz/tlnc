0fb0a1c7ab1373781d2a4b342c5ebaab12cb2807
_ <tsuhaso@gmail.com>
2015-09-23 16:01:19 +0900

    fix jacobian

--- include/tlnc/jacobian.hpp
+++ include/tlnc/jacobian.hpp
@@ -11,28 +11,38 @@
 
 namespace tlnc{
 	namespace detail{
-		template <::std::size_t I, typename ... Exprs>
-		constexpr auto jacobian_impl2(::tlnc::expressions::vector<Exprs...>)
-		{
-			return ::tlnc::expressions::vector<
-				decltype(Exprs{}.template derivative<::tlnc::expressions::vector_arg<I>>())...
-			>{};
-		}
-
-		template <typename ... Exprs, ::sprout::index_t ... Is>
-		constexpr auto jacobian_impl(
+		template <::std::size_t I, typename ... Exprs, ::sprout::index_t ... Is>
+		constexpr auto jacobian_impl2(
 			::tlnc::expressions::vector<Exprs...> v,
 			::sprout::index_tuple<Is...>
 		)
 		{
-			return expressions::matrix<decltype(jacobian_impl2<Is>(v))...>{};
+			using f_type = ::bcl::tuple_element_t<I, typename decltype(v)::tuple_type>;
+			return ::tlnc::expressions::vector<
+				decltype(f_type{}.template derivative<::tlnc::expressions::vector_arg<Is>>())...
+			>{};
+		}
+
+		template <typename ... Exprs, ::sprout::index_t ... Is, ::sprout::index_t ... Js>
+		constexpr auto jacobian_impl(
+			::tlnc::expressions::vector<Exprs...> v,
+			::sprout::index_tuple<Is...>,
+			::sprout::index_tuple<Js...> t
+		)
+		{
+			return expressions::matrix<
+				decltype(jacobian_impl2<Is>(v, t))...
+			>{};
 		}
 	}
 
 	template <::std::size_t N, typename ... Exprs>
 	constexpr auto jacobian(expressions::vector<Exprs...> v)
 	{
-		return detail::jacobian_impl(v, ::sprout::make_index_tuple<N>::make());
+		return detail::jacobian_impl(
+			v,
+			::sprout::make_index_tuple<sizeof...(Exprs)>::make(),
+			::sprout::make_index_tuple<N>::make());
 	}
 
 	template <
@@ -47,7 +57,10 @@ namespace tlnc{
 	template <::std::size_t I, ::std::size_t N, typename ... Exprs>
 	constexpr auto jacobian(expressions::vector<Exprs...> v)
 	{
-		return detail::jacobian_impl(v, ::sprout::index_range<I, I + N>::make());
+		return detail::jacobian_impl(
+			v,
+			::sprout::make_index_tuple<sizeof...(Exprs)>::make(),
+			::sprout::index_range<I, I + N>::make());
 	}
 
 	template <
@@ -62,13 +75,19 @@ namespace tlnc{
 	template <typename ... Exprs, typename ... Xs>
 	constexpr auto jacobian(expressions::vector<Exprs...> v, expressions::vector<Xs...>)
 	{
-		return detail::jacobian_impl(v, ::sprout::index_tuple<Xs::index...>::make());
+		return detail::jacobian_impl(
+			v,
+			::sprout::make_index_tuple<sizeof...(Exprs)>::make(),
+			::sprout::index_tuple<Xs::index...>::make());
 	}
 
 	template <typename ... Exprs, typename X>
 	constexpr auto jacobian(expressions::vector<Exprs...> v, X)
 	{
-		return detail::jacobian_impl(v, ::sprout::index_tuple<X::index>::make());
+		return detail::jacobian_impl(
+			v,
+			::sprout::make_index_tuple<sizeof...(Exprs)>::make(),
+			::sprout::index_tuple<X::index>::make());
 	}
 }
 
